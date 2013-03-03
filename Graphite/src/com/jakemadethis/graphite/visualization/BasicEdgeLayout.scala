a82deb7ab0a4d6dc84dd42fc8b0fffa8bbda22f2
Jake Coxon <jake.coxon@gmail.com>
2013-03-03 20:52:32 +0000

    BasicEdgeLayout now places edge at middle of points

--- Graphite/src/com/jakemadethis/graphite/visualization/BasicEdgeLayout.scala
+++ Graphite/src/com/jakemadethis/graphite/visualization/BasicEdgeLayout.scala
@@ -4,16 +4,17 @@ import com.jakemadethis.graphite.ui.VisualEdge
 import edu.uci.ics.jung.visualization.layout.PersistentLayout.Point
 import edu.uci.ics.jung.algorithms.layout.Layout
 import java.awt.geom.Point2D
+import collection.JavaConversions._
 
 class BasicEdgeLayout[V,E](layout: Layout[V,E]) extends EdgeLayout[E] {
   def getEdgeLocation(e : E) : Point2D = {
-    val es = layout.getGraph().getIncidentVertices(e)
-    if (es.size == 2) {
-      val iterator = layout.getGraph().getIncidentVertices(e).iterator()
-      val p1 = layout.transform(iterator.next())
-      val p2 = layout.transform(iterator.next())
-      return new Point2D.Double((p1.getX() + p2.getX())/2, (p1.getY() + p2.getY())/2);
-    }
-    new Point2D.Double(0,0)
+    val vs = layout.getGraph().getIncidentVertices(e)
+    val ps = vs.map(layout.transform(_))
+    val xs = ps.map(_.getX())
+    val ys = ps.map(_.getY())
+    val midX = (xs.min + xs.max) / 2
+    val midY = (ys.min + ys.max) / 2
+
+    return new Point2D.Double(midX, midY);
   }
 }
