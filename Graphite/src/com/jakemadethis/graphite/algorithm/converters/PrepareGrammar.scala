80ad3e2decc49dc8cc7fbfbd0942489af0686fe0
Jake Coxon <jake.coxon@gmail.com>
2013-04-07 23:06:16 +0100

    Check non-terminal types

--- Graphite/src/com/jakemadethis/graphite/algorithm/converters/PrepareGrammar.scala
+++ Graphite/src/com/jakemadethis/graphite/algorithm/converters/PrepareGrammar.scala
@@ -32,7 +32,38 @@ object PrepareGrammar {
       if (!initialIsHandle(grammar.initial))
         throw new GrammarError("Initial graph must be a handle")
       
-      else grammar
+      checkNonTerminalTypes(grammar)
+      
+      grammar
+    }
+    
+    def checkNonTerminalTypes(grammar : HypergraphGrammar.HG) = {
+      // Make a map of non-terminals to types.
+      val ntMap = grammar.productions.foldLeft(Map[String, Int]()) { case (result, (nt, prod)) =>
+        val extNodes = prod.externalNodes.size
+        if (result.containsKey(nt)) {
+          val existing = result(nt)
+          if (existing != extNodes) 
+            throw new GrammarError("Mismatched type non-terminal '%s': %d and %d".
+                format(nt, existing, extNodes))
+          result
+        }
+        else {
+          result + (nt -> extNodes)
+        }
+      }
+      
+      // Check non-terminals within the graphs
+      for ((nt, prod) <- grammar.productions; edge <- prod.graph.getEdges.filter(_.isNonTerminal)) {
+        val attachmentNodes = prod.graph.getIncidentCount(edge)
+        if (!ntMap.contains(edge.label)) 
+          throw new GrammarError("Invalid non-terminal '%s' in graph".format(edge.label))
+        val existing = ntMap(edge.label)
+        if (existing != attachmentNodes)
+          throw new GrammarError("Mismatched non-terminal '%s' in graph: %d and %d".
+            format(edge.label, existing, attachmentNodes))
+      }
+      grammar
     }
     
     def hasFakeNodes(graph : Hypergraph[Vertex, Hyperedge]) = 
